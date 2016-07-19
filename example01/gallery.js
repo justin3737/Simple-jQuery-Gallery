eb411c4266c1c44c557fe80d29701ccc4b7a7176
Justin <juns3700@gmail.com>
2016-07-19 11:07:42 +0800

    Add example 01

--- example01/gallery.js
+++ example01/gallery.js
@@ -4,19 +4,28 @@ $(function(){
         $prevBtn  = $('.gallery__navIcon--prev'),
         $nextBtn  = $('.gallery__navIcon--next'),
         _len      = $imgItems.length,
-        _currIdx  = 0;
+        _currIdx  = 0,
+        _isLoop   = true;
 
     //--helper--
     function isPrev(id) {
-
+        if (_currIdx === 0) {
+            return id === _len -1;
+        } else {
+            return id === _currIdx -1;
+        }
     }
 
     function isNext(id) {
-        
+         if (_currIdx === _len -1) {
+            return id === 0;
+        } else {
+            return id === _currIdx +1;
+        }
     }
 
     function isActive(id) {
-        
+        return id === _currIdx;
     }
 
     function hasNext() {
@@ -28,7 +37,7 @@ $(function(){
     }
 
     function nextPage() {
-        if(_currIdx < _len) {
+        if(_currIdx < _len - 1) {
             _currIdx++;
         } else {
             _currIdx = 0;
@@ -40,23 +49,34 @@ $(function(){
         if(_currIdx > 0) {
             _currIdx--;
         } else {
-            _currIdx = _len;
+            _currIdx = _len -1;
         }
         return;
     }
 
     function renderView() {
         //render btns 
-        if (!hasNext()) {
+        if (!hasNext() && !_isLoop) {
             $nextBtn.hide();
-        } else if (!hasPrev()) {
+        } else if (!hasPrev() && !_isLoop) {
             $prevBtn.hide();
         } else {
             $nextBtn.show();
             $prevBtn.show();
         }
         //render gallery
-
+        $imgItems.each(function(id, item){
+            $(item).removeClass().addClass('gallery__item gallery__item--hide');
+            if (isPrev(id)) {
+                $(item).addClass('gallery__item--prev');
+            }
+            if (isNext(id)) {
+                $(item).addClass('gallery__item--next');
+            }
+            if (isActive(id)) {
+                $(item).addClass('gallery__item--active');
+            }
+        });
     }
     
     function init() {
@@ -68,6 +88,7 @@ $(function(){
         nextPage();
         renderView();
     });
+
     $prevBtn.on('click', function(){
         prevPage();
         renderView();
