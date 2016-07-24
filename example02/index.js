d3d964a04452231bc3d7b070fd7dda813845dc96
Justin <juns3700@gmail.com>
2016-07-24 14:25:52 +0800

    Add example02

---
+++ example02/index.js
@@ -0,0 +1,11 @@
+//數據資料 應由後台取得
+var data = [
+    {img:1 ,h1: 'Creative',h2:'DUET'},
+    {img:2 ,h1: 'Friendly',h2:'DEVIL'},
+    {img:3 ,h1: 'Tranquilent',h2:'COMPATRIOT'},
+    {img:4 ,h1: 'Insecure',h2:'HUSSLER'},
+    {img:5 ,h1: 'Loving',h2:'REBEL'},
+    {img:6 ,h1: 'Passionate',h2:'SEEKER'},
+    {img:7 ,h1: 'Crazy',h2:'FRIEND'},
+];
+
