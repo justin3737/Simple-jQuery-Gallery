9c979ee12c2449639ddecbeeaf0ddd14f94bb492
Justin <juns3700@gmail.com>
2016-08-13 22:13:03 +0800

    左右分區

--- example03/index.js
+++ example03/index.js
@@ -25,6 +25,28 @@ function random(range) {
     return number;
 }
 
+//計算左右分區的範圍 { left:{x:[min,max], y:[]}, right: {} }
+function rangePos() {
+    var range = { left:  { x:[], y:[] }, right: { x:[], y:[] } };
+    var wrap = {
+        w:g('#wrap').clientWidth,
+        h:g('#wrap').clientHeight
+    }
+
+    var photo = {
+        w:g('.photo')[0].clientWidth,
+        h:g('.photo')[0].clientHeight 
+    }
+
+    range.left.x = [ 0-photo.w, wrap.w/2 - photo.w/2 ];
+    range.left.y = [ 0-photo.h, wrap.h ];
+    
+    range.right.x = [ wrap.w/2 + photo.w/2, wrap.w + photo.w ];
+    range.right.y = range.left.y;   
+
+    return range;
+}   
+
 var data = data;
 
 function addPhotos() {
@@ -48,6 +70,32 @@ addPhotos();
 
 //排序海報
 function rsort( n ) {
+    var _photo = g('.photo');
+    var photos = [];
+    for (s = 0; s < _photo.length; s++ ) {
+        _photo[s].className = _photo[s].className.replace(/\s*photo_center\s*/," ");
+        photos.push(_photo[s]);
+    }
     var photo_center = g('#photo_' + n);
     photo_center.className += ' photo_center '; 
+    photo_center = photos.splice(n, 1);
+    
+    //海報一分為二, 左右兩區
+    var photos_left = photos.splice(0, Math.ceil(photos.length/2));
+    var photos_right = photos;
+    var ranges = rangePos();
+
+    for (var s in photos_left) {
+        var photos = photos_left[s];
+        photos.style.left =  random(ranges.left.x) +'px';
+        photos.style.top  =  random(ranges.left.y) +'px'; 
+        photos.style['-webkit-transform'] = 'rotate(' + random([-150, 150])+ 'deg)';
+    }
+
+    for (var s in photos_right) {
+        var photos = photos_right[s];
+        photos.style.left =  random(ranges.right.x) +'px';
+        photos.style.top  =  random(ranges.right.y) +'px'; 
+        photos.style['-webkit-transform'] = 'rotate(' + random([-150, 150])+ 'deg)';
+    }
 }
